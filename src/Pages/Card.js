1e300fead49c05a86abfb20e635907c608395708
Pranav Patki <66993183+pranav0609@users.noreply.github.com>
2022-03-24 02:58:04 +0530

    rendering doctors from firebase

--- src/Pages/Card.js
+++ src/Pages/Card.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import db from "../firebase";
 //import AddQueue from '../Components/AddQueue'
 //import Navbar from '../Components/Navbar'
+import usercontext from '../Context/usercontext';
 import Card from '@mui/material/Card';
-import { doc, onSnapshot, query,collection, getDocs } from "firebase/firestore";
+import { doc, onSnapshot, query,collection, getDocs, where } from "firebase/firestore";
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
@@ -20,32 +21,38 @@ function Cardfunc() {
         tokenIssued: 0,
         tokenRemaining: 0,
     });
-
+    const context = useContext(usercontext)
+    const { user } = context;
+    const uid = user.uid;
+    console.log(uid);
   React.useEffect(() => {
     async function getData() {
-        // const q = query(collection(db, "admin", "l3jep9KQkHRVGLdjgQplZINctS42"));
-        // doc(q);
-        // //querySnapshot.forEach((doc) => {
-        // // doc.data() is never undefined for query doc snapshots
-        // console.log(doc.data());
-        // });
-      const unsub = onSnapshot(
-        doc(db, "admin", "l3jep9KQkHRVGLdjgQplZINctS42"),
-        { includeMetadataChanges: true },
-        (doc) => {
+        //const q = query(collection(db, "queue"), where("admin_id", "==", uid));
+        //console.log(q);
+        var query = collection(db, "queue")
+        const querySnapshot = await getDocs(query);
+        //console.log(querySnapshot);
+        querySnapshot.forEach((doc) => {
             console.log(doc.data());
-            // setqueueData({
-            //     ...queueData,
-            //     averageWaitingTime: doc.data().Avg_wait_time,
-            //     arrTokens: doc.data().arr_tokens,
-            //     maxTokens: doc.data().max_tokens,
-            //     queueDetails: doc.data().queue_details,
-            //     status: doc.data().status,
-            //     tokenProcessed: doc.data().token_distributed - doc.data().arr_tokens.length,
-            //     tokenIssued: doc.data().token_distributed,
-            //     tokenRemaining: doc.data().max_tokens - doc.data().token_distributed,
-            // });
+            setqueueData({
+                ...queueData,
+                averageWaitingTime: doc.data().Avg_wait_time,
+                arrTokens: doc.data().arr_tokens,
+                maxTokens: doc.data().max_tokens,
+                queueDetails: doc.data().queue_details,
+                status: doc.data().status,
+                tokenProcessed: doc.data().token_distributed - doc.data().arr_tokens.length,
+                tokenIssued: doc.data().token_distributed,
+                tokenRemaining: doc.data().max_tokens - doc.data().token_distributed,
+            });
         });
+    //   const unsub = onSnapshot(
+    //     doc(db, "admin", "l3jep9KQkHRVGLdjgQplZINctS42"),
+    //     { includeMetadataChanges: true },
+    //     (doc) => {
+    //         console.log(doc.data());
+            
+    //     });
       }
   getData();
 }, []);
