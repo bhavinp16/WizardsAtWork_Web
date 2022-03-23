372e638ee9c33f8059d935e6cdb48c167a2b071e
ishi03 <ishita.rai@somaiya.edu>
2022-03-23 15:50:08 +0530

    edited addqueue

--- src/Components/AddQueue.js
+++ src/Components/AddQueue.js
@@ -1,37 +1,92 @@
 import React from "react";
 import db from "../firebase";
 import { AdminInput } from "./AdminInput";
-
+import IconButton from '@mui/material/IconButton';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import { useState,useEffect } from "react";
+import {
+    collection,
+    getDocs,
+    addDoc,
+    updateDoc,
+    deleteDoc,
+    doc,
+  } from "firebase/firestore";
+  
 export default function AddQueue() {
-    const [queue, setQueue] = React.useState([]);
-    const [newQueueName, setNewQueueName] = React.useState();
+    // const [queue, setQueue] = React.useState([]);
+    // const [newQueueName, setNewQueueName] = React.useState();
+    
 
-    React.useEffect(() => {
-        const fetchData = async () => {
-            //const db = firebase.firestore();
-            const data = await db.collection("queue").get();
-            setQueue(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
-        };
-        fetchData();
-    }, []);
+    // React.useEffect(() => {
+    //     const fetchData = async () => {
+    //         //const db = firebase.firestore();
+    //         const data = await db.collection("queue").get();
+    //         setQueue(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+    //     };
+    //     fetchData();
+    // }, []);
 
-    const onCreate = () => {
-        //const db = firebase.firestore();
-        db.collection("queue").add({ name: newQueueName });
+    // const onCreate = () => {
+    //     //const db = firebase.firestore();
+    //     db.collection("queue").add({ name: newQueueName });
+    // };
+    
+    const [newQueue, setNewQueue] = useState("");
+    const [newAge, setNewAge] = useState(0);
+  
+    const [queues, setQueues] = useState({});
+    const QueuesCollectionRef = collection(db, "queue");
+  
+    const createQueue = async () => {
+      await addDoc(QueuesCollectionRef, { Queue: newQueue, age: Number(newAge) });
     };
-
+  
+    const updateQueue = async (id, age) => {
+      const QueueDoc = doc(db, "Queues", id);
+      const newFields = { age: age + 1 };
+      await updateDoc(QueueDoc, newFields);
+    };
+  
+    const deleteQueue = async (id) => {
+      const QueueDoc = doc(db, "Queues", id);
+      await deleteDoc(QueueDoc);
+    };
+  
+    useEffect(() => {
+      const getQueues = async () => {
+        const data = await getDocs(QueuesCollectionRef);
+        setQueues(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        console.log(queues);
+        console.log(data);
+      };
+  
+      getQueues();
+    }, []);
+  
     return (
         <ul>
             <input
-                value={newQueueName}
-                onChange={e => setNewQueueName(e.target.value)}
+                value={newQueue}
+                onChange={e => setNewQueue(e.target.value)}
             />
-            <button onClick={onCreate}>Create</button>
-            {queue.map(q => (
-                <li key={q.name}>
+            {/* <button onClick={onCreate}>Create</button> */}
+            <IconButton
+            onClick={createQueue}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="add"
+            sx={{ mr: 2 }}
+
+          >
+            <AddBoxIcon />
+          </IconButton>
+            {/* {queue.map(q => (
+                <li key={q.Queue}>
                     <AdminInput q={q} />
                 </li>
-            ))}
+            ))} */}
         </ul>
     );
 }
