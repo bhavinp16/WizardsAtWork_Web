23c6a654dbe334901ede78eae007b0f7b2d767d0
ishi03 <ishita.rai@somaiya.edu>
2022-03-24 12:34:52 +0530

    "geoloc fn for future use"

---
+++ src/Components/Geoloc.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+
+function getLoc(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        console.log("Geolocation is not supported by this browser.");
+      }
+}
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude);
+
+    return {"Latitude": position.coords.latitude,
+    "Longitude": position.coords.longitude};
+  }
+
+function Geoloc() {
+    return (
+        <Button onClick={getLoc}>
+            GeoLocation
+        </Button>
+            
+    )
+}
+
+export default Geoloc
